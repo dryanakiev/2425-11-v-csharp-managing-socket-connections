674014a0bfa3066e935da04c75d9530262a3f708
dryanakiev <dyanakiev@codingburgas.bg>
2025-02-07 10:22:34 +0200

    Add client and server socket example

---
+++ MySocketConnection/MySocketConnection.Client/Client.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace MySocketConnection.Client;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            string input = Console.ReadLine();
+            
+            buffer = Encoding.ASCII.GetBytes(input);
+            
+            clientSocket.Send(buffer);
+        }
+    }
+}
---
+++ MySocketConnection/MySocketConnection.Server/Server.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MySocketConnection.Server;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        IPAddress serverIp = IPAddress.Parse("");
+        
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        byte[] buffer = new byte[1024];
+
+        while (true)
+        {
+            int bytesRead = serverSocket.Receive(buffer);
+
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+        }
+    }
+}

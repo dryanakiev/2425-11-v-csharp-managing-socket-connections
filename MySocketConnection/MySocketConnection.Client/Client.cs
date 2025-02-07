518279495f3ab01f2240ef8cce94b9b917818682
dryanakiev <dyanakiev@codingburgas.bg>
2025-02-07 10:31:36 +0200

    Add IPEndpoints

--- MySocketConnection/MySocketConnection.Client/Client.cs
+++ MySocketConnection/MySocketConnection.Client/Client.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Sockets;
+﻿using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace MySocketConnection.Client;
@@ -8,6 +9,8 @@ class Client
     static void Main(string[] args)
     {
         Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        clientSocket.Connect(new IPEndPoint(IPAddress.Loopback, 8080));
         
         byte[] buffer = new byte[1024];
 
--- MySocketConnection/MySocketConnection.Server/Server.cs
+++ MySocketConnection/MySocketConnection.Server/Server.cs
@@ -8,17 +8,25 @@ class Server
 {
     static void Main(string[] args)
     {
-        IPAddress serverIp = IPAddress.Parse("");
+        IPAddress serverIp = IPAddress.Any;
         
         Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        serverSocket.Bind(new IPEndPoint(serverIp, 8080));
+        
+        serverSocket.Listen(10);
+        
+        Socket clientSocket = serverSocket.Accept();
         
         byte[] buffer = new byte[1024];
 
         while (true)
         {
-            int bytesRead = serverSocket.Receive(buffer);
+            int bytesRead = clientSocket.Receive(buffer);
 
-            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            string messageReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            
+            Console.WriteLine($"Client sent: {messageReceived}");
         }
     }
 }

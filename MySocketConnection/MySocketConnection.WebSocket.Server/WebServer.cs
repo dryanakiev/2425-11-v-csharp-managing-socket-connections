a3dd5891f9defc9989ebe0e1e89dcf443d8e8c30
dryanakiev <dyanakiev@codingburgas.bg>
2025-02-20 22:40:59 +0200

    Add external html and css to public root directory

--- MySocketConnection/MySocketConnection.WebSocket.Server/WebServer.cs
+++ MySocketConnection/MySocketConnection.WebSocket.Server/WebServer.cs
@@ -10,7 +10,7 @@ namespace MySocketConnection.WebSocket.Server
 
         public WebServer()
         {
-            UrlPrefix = "http://localhost:5000/";
+            UrlPrefix = "http://localhost:9999/";
             Listener = new HttpListener();
             Listener.Prefixes.Add(UrlPrefix);
             Listener.Start();
@@ -30,14 +30,39 @@ namespace MySocketConnection.WebSocket.Server
         {
             string path = context.Request.Url.AbsolutePath;
             string responseString = " ";
+            string rootDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+            
+            string userIP = context.Request.RemoteEndPoint?.Address.ToString() ?? "Unknown IP";
+            Console.WriteLine($"User {userIP} is visiting: {path}");
 
             if (path == "/")
             {
-                responseString = "<html><body><h2>Welcome to the Web Server!</h2></body></html>";
+                string indexPath = Path.Combine(rootDirectory, "index.html");
+                responseString = File.ReadAllText(indexPath);
+                context.Response.ContentType = "text/html";
+            }
+            else if (path == "/about")
+            {
+                string aboutPath = Path.Combine(rootDirectory, "about.html");
+                responseString = File.ReadAllText(aboutPath);
+                context.Response.ContentType = "text/html";
+            }
+            else if (path == "/contact")
+            {
+                string contactPath = Path.Combine(rootDirectory, "contact.html");
+                responseString = File.ReadAllText(contactPath);
+                context.Response.ContentType = "text/html";
+            }
+            else if (path.StartsWith("/css/"))
+            {
+                string cssFilePath = Path.Combine(rootDirectory, path.TrimStart('/'));
+                responseString = File.ReadAllText(cssFilePath);
+                context.Response.ContentType = "text/css";
             }
             else
             {
-                responseString = "<html><body><h2>404 - Not Found</h2></body></html>";
+                responseString = "<html><body><img src=\"https://i0.wp.com/wasmormon.org/wp-content/uploads/2023/04/image.png?resize=498%2C256&ssl=1\"></img></body></html>";
+                context.Response.ContentType = "text/html";
             }
 
 
